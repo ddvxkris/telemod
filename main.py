e34611bb717c14a0f5b539d67d3a6dce128c94c7
ddvxkris <socialnet.ddevx.kris@gmail.com>
2022-08-17 00:17:42 +0300

    Добавлена /ban команда

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from time import time
+from threading import Thread
+from time import sleep, time
 try:
     import telebot
     from telebot import types
@@ -34,22 +35,32 @@ def handle_user(func):
     return wrapper
 
 if __name__ == '__main__':
-    bot = telebot.TeleBot('yourtokenhere')
+    bot = telebot.TeleBot('5149939704:AAEWTPpQHm_6tf7FmT6BL-4ir92ryJZqWug')
+
+    # отвечает на команду, а через некоторое время удаляет ответ и команду (чтобы не засорять чат)
+    def reply_and_delete(message: types.Message, text: str):
+        def reply_and_delete_thread():
+            nonlocal message, text
+            bot_message = bot.reply_to(message, text)
+            sleep(10)
+            bot.delete_message(bot_message.chat.id, bot_message.id)
+            bot.delete_message(bot_message.reply_to_message.chat.id, bot_message.reply_to_message.id)
+        Thread(target=reply_and_delete_thread()).start()
 
     @bot.message_handler(commands=['report'])
     @handle_user
     def report(message: types.Message):
         if message.reply_to_message == None:
-            bot.reply_to(message, 'Должно быть ответом на сообщение!')
+            reply_and_delete(message, 'Должно быть ответом на сообщение!')
         elif message.reply_to_message.from_user.username == message.from_user.username or message.reply_to_message.from_user.is_bot:
-            bot.send_message(message.chat.id, 'Хватит дурачиться.', reply_to_message_id=message.id)
+            bot.send_message(message.chat.id, 'Хватит дурачиться.', reply_and_delete_message_id=message.id)
         else:
             user_index = find_user_data_index(message.reply_to_message.from_user.username)
             if not user_datas[user_index].reported_by.__contains__(message.from_user.username):
                 user_datas[user_index].reports += 1
                 user_datas[user_index].reported_messages.append(message.reply_to_message)
                 user_datas[user_index].reported_by.append(message.from_user.username)
-                bot.reply_to(message, 'Репорт отправлен')
+                reply_and_delete(message, 'Репорт отправлен')
                 if user_datas[user_index].reports >= 2:
                     bot.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id, until_date=time()+100)
                     
@@ -63,8 +74,23 @@ if __name__ == '__main__':
                     user_datas[user_index].reported_messages = []
                     user_datas[user_index].reported_by = []
             else:
-                bot.reply_to(message, 'Вы уже отправляли репорт на этого пользователя')
+                reply_and_delete(message, 'Вы уже отправляли репорт на этого пользователя')
  
+    @bot.message_handler(commands=['ban'])
+    @handle_user
+    def ban(message: types.Message):
+        status = bot.get_chat_member(message.chat.id, message.from_user.id).status
+        if message.reply_to_message == None:
+            reply_and_delete(message, 'Должна быть ответом на сообщение!')
+        elif status != 'creator' and status != 'administrator':
+            reply_and_delete(message, 'Недостаточно прав!')
+        else:
+            try:
+                bot.ban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
+                reply_and_delete(message, 'Участник успешно забанен!')
+            except:
+                reply_and_delete(message, 'Не удалось забанить участника.')
+
     @bot.message_handler()
     @handle_user
     def message_handling(message: types.Message):
